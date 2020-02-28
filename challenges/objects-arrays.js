46834d1654f2f9a607bca79961a3bda354da5023
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-02-28 11:16:00 -0800

    Made edits to streamline code

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -129,7 +129,7 @@ The zoos want to display both the scientific name and the animal name in front o
 */
 const displayNames = [];
 
-zooAnimals.forEach((item) => {return displayNames.push(`Name: ${item.animal_name}, Scientific: ${item.scientific_name}`);})
+zooAnimals.forEach((item) => displayNames.push(`Name: ${item.animal_name}, Scientific: ${item.scientific_name}`))
 
 console.log(displayNames);
 
@@ -139,7 +139,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map((item) => {return item.animal_name.toLowerCase();})
+const lowCaseAnimalNames = zooAnimals.map((item) => item.animal_name.toLowerCase())
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -147,7 +147,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = zooAnimals.filter((item) => {return item.population < 5});
+const lowPopulationAnimals = zooAnimals.filter((item) => item.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -155,7 +155,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = zooAnimals.reduce((accumulator, item) => {return accumulator + item.population}, 0);
+const populationTotal = zooAnimals.reduce((accumulator, item) => accumulator + item.population, 0);
 console.log(populationTotal);
 
 
